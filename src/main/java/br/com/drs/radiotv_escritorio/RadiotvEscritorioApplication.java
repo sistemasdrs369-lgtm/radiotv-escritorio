1c16a4ed6d6abcf9cdb93a896fb76fd1745c5028
DRS <sistemacontrolematter@gmail.com>
2026-01-23 09:02:38 -0300

    Inicial commit

---
+++ src/main/java/br/com/drs/radiotv_escritorio/RadiotvEscritorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.drs.radiotv_escritorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiotvEscritorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiotvEscritorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/drs/radiotv_escritorio/RadiotvEscritorioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drs.radiotv_escritorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiotvEscritorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
