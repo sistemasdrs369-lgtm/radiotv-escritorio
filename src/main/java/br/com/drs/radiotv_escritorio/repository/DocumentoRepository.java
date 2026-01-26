3d54bba1dfd6f816f1e63d8fd5194ba8a1d31eb8
DRS <sistemacontrolematter@gmail.com>
2026-01-26 11:10:05 -0300

    Apagar

--- src/main/java/br/com/drs/radiotv_escritorio/repository/DocumentoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.model.Documento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentoRepository extends JpaRepository<Documento, Long> {
-
-}
--- src/main/java/br/com/drs/radiotv_escritorio/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
