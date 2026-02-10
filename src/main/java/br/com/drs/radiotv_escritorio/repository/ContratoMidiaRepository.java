3886695d7a26fddab80d544267b1bc7c34415b45
DRS <sistemacontrolematter@gmail.com>
2026-02-10 16:01:33 -0300

    Apagar

--- src/main/java/br/com/drs/radiotv_escritorio/repository/ContratoMidiaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.model.ContratoMidia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContratoMidiaRepository extends JpaRepository<ContratoMidia, Long> {
-}
