6b3b4808e3944ca86260355c5fed44616544cfe4
DRS <sistemacontrolematter@gmail.com>
2026-02-11 10:19:33 -0300

    Apagar

--- src/main/java/br/com/drs/radiotv_escritorio/repository/AgenciadorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.model.Agenciador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgenciadorRepository extends JpaRepository<Agenciador, Long> {
-}
--- src/main/java/br/com/drs/radiotv_escritorio/repository/ContratoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.model.Contrato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContratoRepository extends JpaRepository<Contrato, Long> {
-}
