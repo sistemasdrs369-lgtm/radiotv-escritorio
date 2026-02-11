de724af61d373bd7efeedb4f621cd421a3a1d118
DRS <sistemacontrolematter@gmail.com>
2026-02-11 10:54:07 -0300

    Enum Sexo

---
+++ src/main/java/br/com/drs/radiotv_escritorio/model/enuns/Sexo.java
@@ -0,0 +1,15 @@
+package br.com.drs.radiotv_escritorio.model.enuns;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Sexo {
+
+    MASCULINO("Masculino"),
+    FEMININO("Feminino"),
+    OUTRO("Outro");
+
+    private final String descricao;
+}
