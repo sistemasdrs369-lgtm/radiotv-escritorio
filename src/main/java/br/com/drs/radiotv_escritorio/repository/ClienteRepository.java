4d731c723cdd2f510c89bb2724c4c8d4bfe6ef77
DRS <sistemacontrolematter@gmail.com>
2026-02-11 09:29:56 -0300

    Apagar

--- src/main/java/br/com/drs/radiotv_escritorio/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.drs.radiotv_escritorio.repository;
-
-import br.com.drs.radiotv_escritorio.dto.ClienteDTO;
-import br.com.drs.radiotv_escritorio.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-    ClienteDTO findByNomeFantasia(String nomeFantasia);
-}
